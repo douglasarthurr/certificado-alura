f6e94e1c03e60c372662815b4ecf3f30c0ea5bc8
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2023-02-03 17:31:17 -0400

    Atualização da pagina

--- aula 1 conversor  de moedas/script.js
+++ aula 1 conversor  de moedas/script.js
@@ -2,9 +2,7 @@
 // Adicionar junto ao alert o nome da pessoa que esta pedindo o valor
 // Converter duas unidades, anos luz em metros. alterar background e tornar um conversor interestrelar 
 
-let valueToConvert = 30;
-const dollarPrice = 5.32;
-
-let convertedValue = valueToConvert * dollarPrice;
-convertedValue = convertedValue.toFixed(2)
-console.log('R$: '+ convertedValue);
+let input = document.getElementById("inputNumber")
+function buttonConvert(){
+    console.log(input)
+}
