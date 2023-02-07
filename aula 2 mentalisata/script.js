dd865cf8e66970ef7f96617dabbe695eba14ba97
Douglas bento <douglasarthur.s.bento@gmail.com>
2023-02-06 21:57:29 -0400

    mentalista finalizado

--- aula 2 mentalisata/script.js
+++ aula 2 mentalisata/script.js
@@ -3,18 +3,67 @@
 //Zero não apareca seja um numero entre 1 e 1000
 //Quantas tentativas tem que ser feita para acertar
 
-const secretNumber = parseInt(Math.random() * 1001);
+this.secretNumber = Math.floor(Math.random() * 1000) + 1;
+const InputNumber = document.getElementById("inputNumber");
+const textNumber = document.getElementById("textNumber")
+const buttonStart = document.getElementById("buttonStart");
+const kicks = document.getElementById("kicks");
+let numberKicks = 0;
 
-let InputNumber = 0;
 
-while (InputNumber != secretNumber){
-    InputNumber = prompt("diz ai");
+function start(){
+    InputNumber.removeAttribute("disabled");
+    buttonStart.removeAttribute("onclick");
+    buttonStart.setAttribute("onclick", "buttonKicks()");
+    buttonStart.textContent = "chutar";
+    pairOrOdd();
+}
 
-    if(InputNumber == secretNumber){
-        alert("Acertou")
-    }else if(InputNumber > secretNumber){
-        alert("Errou! o numero secreto é menor")
-    }else if(InputNumber < secretNumber){
-        alert("Errou! o numero secreto é maior")
+function buttonKicks(){
+    numberKicks++;
+    kicks.textContent = "Chutes: " + numberKicks;
+    buttonStart.textContent = "chutar";
+    number = InputNumber.value;
+    if(number > 1000 || number <= 0 || (isNaN(number))){
+        InputNumber.value = null;
+        InputNumber.setAttribute("disabled", "disabled");
+        textNumber.textContent = "Número inválido, jogue novamente";
+        buttonStart.removeAttribute("onclick");
+        buttonStart.setAttribute("onclick", "start()");
+        buttonStart.textContent = "começar";
+        numberKicks = 0;
+        kicks.textContent = "Chutes: " + numberKicks;
+
+    }else if(number < this.secretNumber){
+        textNumber.textContent = "Errou! o numero secreto é maior";
+        
+    }else if(number > this.secretNumber){
+        textNumber.textContent = "Errou! o numero secreto é menor";
+        
+    }else if(number == secretNumber){
+        textNumber.textContent = "Parabéns você acertou o número " + secretNumber;
+        InputNumber.setAttribute("disabled", "disabled")
+        buttonStart.removeAttribute("onclick")
+        buttonStart.setAttribute("onclick", "restart()")
+        buttonStart.textContent = "recomeçar";
     }
+}
+
+function pairOrOdd(){
+    if(this.secretNumber%2 === 0){
+        textNumber.textContent = "Número secreto é par";
+    }else{
+        textNumber.textContent = "Número secreto é impar";
+    };
+};
+
+function restart(){
+    this.secretNumber = Math.floor(Math.random() * 1000) + 1;
+    InputNumber.value = null;
+    buttonStart.removeAttribute("onclick");
+    buttonStart.setAttribute("onclick", "start()");
+    textNumber.textContent = "Digite um número de 1 a 1000";
+    numberKicks = 0;
+    kicks.textContent = "Chutes: " + numberKicks;
+    start();
 }
